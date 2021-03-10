6dc643b8401565dec9cc963c6b7c6fa067e01d4c
3200110922 <79961822+3200110922@users.noreply.github.com>
2021-03-10 16:01:04 +0800

    Add files via upload

---
+++ Practical5/fibonacci.py
@@ -0,0 +1,8 @@
+
+a=1
+b=1
+for i in range (1,12):
+        c = a + b
+        a = b
+        b = c
+        print(c)
---
+++ Practical5/r_rate.py
@@ -0,0 +1,7 @@
+r=1.2
+n=84
+for i in range (1,6):
+        n=n*r
+print('the r rate is', r ,', the infected students is', n)
+
+
