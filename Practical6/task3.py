b295e442e18392273a641e3560ba2c603702e56c
3200110922 <79961822+3200110922@users.noreply.github.com>
2021-03-17 14:09:35 +0800

    Add files via upload

---
+++ Practical6/task3.py
@@ -0,0 +1,24 @@
+gene_lengths = [9410,394141,4442,105338,19149,76779,126550,36296,842,15981]
+exon_counts = [51,1142,42,216,25,650,32533,57,1,523]
+
+import numpy as np
+import matplotlib.pyplot as plt
+
+a = np.array(gene_lengths)
+b = np.array(exon_counts)
+c = a/b
+
+c.sort()
+
+plt.boxplot(c,
+    vert=True,
+    whis=1.5,
+    patch_artist=True,
+    meanline=False,
+    showbox=True,
+    showcaps=True,
+    showfliers=True,
+    notch=False
+    )
+plt.show()
+print(c)
