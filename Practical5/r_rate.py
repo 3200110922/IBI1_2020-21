9ac944724fb848135a1fa13e4c4fe4a41c6ab521
3200110922 <79961822+3200110922@users.noreply.github.com>
2021-03-18 16:19:40 +0800

    Add files via upload

--- Practical5/r_rate.py
+++ Practical5/r_rate.py
@@ -1,7 +1,8 @@
-r=1.2
-n=84
+r=1.2 # infection rate
+n=84 # cardinal number
 for i in range (1,6):
-        n=n*r
+        n=n*r + n
+        # in this circulation, n multiply r constantly then plus previous n so that the ultimate number will be given
 print('the r rate is', r ,', the infected students is', n)
 
 
