76dfe5978202ffd994546c3c93cb13e3c3075a6c
3200110922 <79961822+3200110922@users.noreply.github.com>
2021-03-18 16:21:02 +0800

    Update fibonacci.py

--- Practical5/fibonacci.py
+++ Practical5/fibonacci.py
@@ -2,7 +2,8 @@
 a=1
 b=1
 for i in range (1,12):
-        c = a + b
-        a = b
-        b = c
-        print(c)
+        c = a + b # define a new variable that equals to a plus b
+        a = b # make a equal to b
+        b = c # make b equal to c, so that in next circulation new c will be superposed.
+        print(c) # c is the result
+
