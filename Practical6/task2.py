f556a67749b0a3ce087569f6a9f68798fbdcc83e
3200110922 <79961822+3200110922@users.noreply.github.com>
2021-05-17 10:14:54 +0800

    Add files via upload

--- Practical6/task2.py
+++ Practical6/task2.py
@@ -1,9 +1,17 @@
 import matplotlib.pyplot as plt
 
-biaoqian = 'USA','India','Brazil','Russia','UK' # set up a labels called 'biaoqian'
-bili = [29862124,11285561,11205972,4360823,4234924] # set up a list called 'bili'
-explode = (0.1,0.1,0.1,0.1,0.1) # make every part's interspace 0.1
-plt.pie(bili, explode=explode, labels=biaoqian,autopct='%1.1f%%', shadow= False, startangle=90) # set up a new pie graph 
-plt.axis('equal') # Make the scales equal in length
+biaoqian = 'USA','India','Brazil','Russia','UK'
+bili = [29862124,11285561,11205972,4360823,4234924]
+explode = (0.1,0.1,0.1,0.1,0.1)
+plt.pie(bili, explode=explode, labels=biaoqian,autopct='%1.1f%%', shadow= False, startangle=90)
+plt.axis('equal')
+plt.title('example:Comparing coronavirus infection rates across countries')
 plt.show()
-
+for i in range(1,6):
+    change=[]
+    n = int(input())
+    change.append(n)
+plt.pie(bili, explode=explode, labels=biaoqian,autopct='%1.1f%%', shadow= False, startangle=90)
+plt.axis('equal')
+plt.title('change:Comparing coronavirus infection rates across countries')
+plt.show()
